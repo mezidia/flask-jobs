23887420332807a4372d1f7dfd4c69d689312ee9
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-10 09:56:09 +0300

    Add config file

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+from config import Configuration
+
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object(Configuration)
 
 @app.route('/')
 def index():
     return 'Hello world!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
