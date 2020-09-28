799fc3166fe1e94024f3acea40428c41c0616015
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-28 18:40:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -35,12 +35,14 @@ users = {
     os.environ.get('LOGIN'): generate_password_hash(os.environ.get('PASSWORD')),
 }
 
+
 # Verification function
 @auth.verify_password
 def verify_password(username, password):
     if username in users and \
             check_password_hash(users.get(username), password):
         return username
+
 
 # Job Class/Model
 class Job(db.Model):
